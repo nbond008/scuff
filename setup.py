23e6a595c6b4ceca1f8663f44435c3d92ea29804
Nick Bond <nickjb1234@gmail.com>
2018-04-21 20:20:37 -0400

    Issue #12 - added pip metadata

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name         = 'scuff',
+    version      = '0.3',
+    description  = 'The Innovation Express capstone project',
+    url          = 'https://github.com/nbond008/scuff',
+    author       = 'Nick Bond and Gabe Waksman',
+    author_email = 'the.innovation.express@gmail.com',
+    license      = 'Georgia Tech',
+    packages     = ['.'],
+    zip_safe     = False
+)
